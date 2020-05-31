9a8353e8aec74fc9066fc26b7ce5cc34f89faf6c
Lizaveta <ElizavetaBun@gmail.com>
2020-05-31 19:03:13 +0400

    map style applied

--- main.js
+++ main.js
@@ -20,32 +20,7 @@ var margin = { top: 0, right: 50, bottom: 0, left: 0 },
   width = 960 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
-var color = d3
-  .scaleThreshold()
-  .domain([
-    10000,
-    100000,
-    500000,
-    1000000,
-    5000000,
-    10000000,
-    50000000,
-    100000000,
-    500000000,
-    1500000000,
-  ])
-  .range([
-    "rgb(247,251,255)",
-    "rgb(222,235,247)",
-    "rgb(198,219,239)",
-    "rgb(158,202,225)",
-    "rgb(107,174,214)",
-    "rgb(66,146,198)",
-    "rgb(33,113,181)",
-    "rgb(8,81,156)",
-    "rgb(8,48,107)",
-    "rgb(3,19,43)",
-  ]);
+var color = d3.scaleThreshold();
 
 var path = d3.geoPath();
 
@@ -59,7 +34,7 @@ var svg = d3
 
 var projection = d3
   .geoMercator()
-  .scale(130)
+  .scale(140)
   .translate([width / 2, height / 1.5]);
 
 var path = d3.geoPath().projection(projection);
@@ -92,26 +67,26 @@ function ready(error, data, population) {
     .style("fill", function (d) {
       return color(populationById[d.id]);
     })
-    .style("stroke", "white")
+    .style("stroke", "rgb(163, 230, 204)")
     .style("stroke-width", 1.5)
-    .style("opacity", 0.8)
+    .style("opacity", 0.7)
     // tooltips
-    .style("stroke", "white")
+    .style("stroke", "rgb(163, 230, 204)")
     .style("stroke-width", 0.3)
     .on("mouseover", function (d) {
       tip.show(d);
 
       d3.select(this)
-        .style("opacity", 1)
-        .style("stroke", "white")
+        .style("opacity", 0.9)
+        .style("stroke", "rgb(163, 230, 204)")
         .style("stroke-width", 3);
     })
     .on("mouseout", function (d) {
       tip.hide(d);
 
       d3.select(this)
-        .style("opacity", 0.8)
-        .style("stroke", "white")
+        .style("opacity", 0.7)
+        .style("stroke", "rgb(163, 230, 204)")
         .style("stroke-width", 0.3);
     });
 
